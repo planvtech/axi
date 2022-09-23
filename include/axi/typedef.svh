853909a23710f6fce1115e5e483d9ab67eed833a
BasitSajjad <abdul.basit@planv.tech>
2022-09-23 07:26:25 +0200

    added snoop signals in AR,R,AW

--- include/axi/typedef.svh
+++ include/axi/typedef.svh
@@ -207,5 +207,103 @@
   `AXI_LITE_TYPEDEF_RESP_T(__name``_resp_t, __name``_b_chan_t, __name``_r_chan_t)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// AXI4+ATOP Channel and Request/Response Structs (with snoop support)
+//
+// Usage Example:
+// `AXI_TYPEDEF_S_AW_CHAN_T(axi_s_aw_t, axi_addr_t, axi_id_t, axi_user_t)
+// `AXI_TYPEDEF_W_CHAN_T(axi_w_t, axi_data_t, axi_strb_t, axi_user_t)
+// `AXI_TYPEDEF_B_CHAN_T(axi_b_t, axi_id_t, axi_user_t)
+// `AXI_TYPEDEF_S_AR_CHAN_T(axi_s_ar_t, axi_addr_t, axi_id_t, axi_user_t)
+// `AXI_TYPEDEF_S_R_CHAN_T(axi_s_r_t, axi_data_t, axi_id_t, axi_user_t)
+// `AXI_TYPEDEF_S_REQ_T(axi_s_req_t, axi_s_aw_t, axi_w_t, axi_s_ar_t)
+// `AXI_TYPEDEF_S_RESP_T(axi_s_resp_t, axi_b_t, axi_s_r_t)
+`define AXI_TYPEDEF_S_AW_CHAN_T(s_aw_chan_t, addr_t, id_t, user_t)  \
+  typedef struct packed {                                       \
+    id_t                id;                                       \
+    addr_t              addr;                                     \
+    axi_pkg::len_t      len;                                      \
+    axi_pkg::size_t     size;                                     \
+    axi_pkg::burst_t    burst;                                    \
+    logic               lock;                                     \
+    axi_pkg::cache_t    cache;                                    \
+    axi_pkg::prot_t     prot;                                     \
+    axi_pkg::qos_t      qos;                                      \
+    axi_pkg::region_t   region;                                   \
+    axi_pkg::atop_t     atop;                                     \
+    user_t              user;                                     \
+    axi_pkg::awsnoop_t  awsnoop;                                  \
+    axi_pkg::bar_t      bar;                                      \
+    axi_pkg::domain_t   domain;                                   \
+    axi_pkg::awunique_t awunique;                                 \
+  } s_aw_chan_t;
+`define AXI_TYPEDEF_S_AR_CHAN_T(s_ar_chan_t, addr_t, id_t, user_t)  \
+  typedef struct packed {                                         \
+    id_t                id;                                       \
+    addr_t              addr;                                     \
+    axi_pkg::len_t      len;                                      \
+    axi_pkg::size_t     size;                                     \
+    axi_pkg::burst_t    burst;                                    \
+    logic               lock;                                     \
+    axi_pkg::cache_t    cache;                                    \
+    axi_pkg::prot_t     prot;                                     \
+    axi_pkg::qos_t      qos;                                      \
+    axi_pkg::region_t   region;                                   \
+    user_t              user;                                     \
+    axi_pkg::arsnoop_t  arsnoop;                                  \
+    axi_pkg::bar_t      bar;                                      \
+    axi_pkg::domain_t   domain;                                   \
+  } s_ar_chan_t;
+`define AXI_TYPEDEF_S_R_CHAN_T(s_r_chan_t, data_t, id_t, user_t)  \
+  typedef struct packed {                                        \
+    id_t              id;                                       \
+    data_t            data;                                     \
+    axi_pkg::rresp_t  resp;                                    \
+    logic             last;                                     \
+    user_t            user;                                     \
+  } s_r_chan_t;
+`define AXI_TYPEDEF_S_REQ_T(s_req_t, s_aw_chan_t, w_chan_t, s_ar_chan_t)  \
+  typedef struct packed {                                         \
+    s_aw_chan_t aw;                                             \
+    logic     aw_valid;                                           \
+    w_chan_t  w;                                                  \
+    logic     w_valid;                                            \
+    logic     b_ready;                                            \
+    s_ar_chan_t ar;                                             \
+    logic     ar_valid;                                           \
+    logic     r_ready;                                            \
+  } s_req_t;
+`define AXI_TYPEDEF_S_RESP_T(s_resp_t, b_chan_t, s_r_chan_t)  \
+  typedef struct packed {                               \
+    logic     aw_ready;                                 \
+    logic     ar_ready;                                 \
+    logic     w_ready;                                  \
+    logic     b_valid;                                  \
+    b_chan_t  b;                                        \
+    logic     r_valid;                                  \
+    s_r_chan_t  r;                                        \
+  } s_resp_t;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// All AXI4+ATOP Channels and Request/Response Structs in One Macro (with snoop support)
+//
+// This can be used whenever the user is not interested in "precise" control of the naming of the
+// individual channels.
+//
+// Usage Example:
+// `AXI_TYPEDEF_ALL(axi, addr_t, id_t, data_t, strb_t, user_t)
+//
+// This defines `axi_req_t` and `axi_resp_t` request/response structs as well as `axi_aw_chan_t`,
+// `axi_w_chan_t`, `axi_b_chan_t`, `axi_ar_chan_t`, and `axi_r_chan_t` channel structs.
+`define AXI_S_TYPEDEF_ALL(__name, __addr_t, __id_t, __data_t, __strb_t, __user_t)                 \
+  `AXI_TYPEDEF_S_AW_CHAN_T(__name``_s_aw_chan_t, __addr_t, __id_t, __user_t)                    \
+  `AXI_TYPEDEF_W_CHAN_T(__name``_w_chan_t, __data_t, __strb_t, __user_t)                        \
+  `AXI_TYPEDEF_B_CHAN_T(__name``_b_chan_t, __id_t, __user_t)                                    \
+  `AXI_TYPEDEF_S_AR_CHAN_T(__name``_s_ar_chan_t, __addr_t, __id_t, __user_t)                    \
+  `AXI_TYPEDEF_S_R_CHAN_T(__name``_s_r_chan_t, __data_t, __id_t, __user_t)                      \
+  `AXI_TYPEDEF_S_REQ_T(__name``_s_req_t, __name``_s_aw_chan_t, __name``_w_chan_t, __name``_s_ar_chan_t) \
+  `AXI_TYPEDEF_S_RESP_T(__name``_s_resp_t, __name``_b_chan_t, __name``_s_r_chan_t)
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 `endif
